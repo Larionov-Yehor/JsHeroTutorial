3b6d43f160dc0bc0519a6d3e5e651aace7ce9fb3
yehor <yehorl@softwareplanetgroup.com>
2016-12-01 12:31:27 +0200

    display all heros names by using *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   })
 export class AppComponent{
-
   hero: Hero = {
     id:1,
     name: 'Kent'
   }
-
+  heroes: Hero[] = HEROES;
 }
+
 export class Hero {
   id: number;
   name: string;
 }
 
+const HEROES: Hero[]=[
+  {id: 2, name: 'Hlopec'},
+  {id: 3, name: 'Pacanchik'},
+  {id: 4, name: 'Bebchik'}
+];
